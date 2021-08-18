3d13ecc663ff6d8b217b78c8f87afbb21523ff04
jms0923 <37008964+jms0923@users.noreply.github.com>
2021-08-18 10:59:08 +0900

    Create solution.py

---
+++ programmers/프린터/solution.py
@@ -0,0 +1,14 @@
+def solution(priorities, location):
+    answer = 0
+    while priorities:
+        if priorities[0] == max(priorities):
+            priorities.pop(0)
+            answer += 1
+            if location == 0:
+                break    
+        else:
+            priorities.append(priorities.pop(0))
+
+        location = location-1 if location != 0 else len(priorities)-1
+            
+    return answer
