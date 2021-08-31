95e9d9a451642f50f82e36bc48e64c1162f7b312
jms0923 <37008964+jms0923@users.noreply.github.com>
2021-08-31 15:03:44 +0900

    Create solution.py

---
+++ programmers/가장 큰 수/solution.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+    numbers = [str(num) for num in numbers]
+    numbers.sort(key= lambda num: num*3, reverse=True)
+    answer = str(int(''.join(numbers)))
+    
+    return answer
