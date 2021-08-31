a88967b4d45d9ddde8433a281e67b2a31370d00f
jms0923 <37008964+jms0923@users.noreply.github.com>
2021-08-31 15:04:02 +0900

    Update solution.py

--- programmers/가장 큰 수/solution.py
+++ programmers/가장 큰 수/solution.py
@@ -1,6 +1,6 @@
 def solution(numbers):
     numbers = [str(num) for num in numbers]
     numbers.sort(key= lambda num: num*3, reverse=True)
-    answer = str(int(''.join(numbers)))
+    answer = str(int(''.join(numbers))) # remove zero
     
     return answer
